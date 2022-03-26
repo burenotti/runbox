b72efae446f25de66c89c0a4de66feb561e2bd83
Artem Burenin <burenotti@gmail.com>
2022-03-26 15:15:10 +0300

    🚧 Docker Api related changes:
    - Made workdir method of DockerExecutor class
    - Implemented methods to put data in containers (with tests)
    - Naively implemented methods to create and run containers

--- runbox/docker/docker_api.py
+++ runbox/docker/docker_api.py
@@ -1,38 +1,24 @@
+import asyncio
+import io
+import pathlib
+import tarfile
+import uuid
+import time
 from datetime import timedelta
 from typing import Awaitable, Callable, Literal, Sequence
 from aiodocker import Docker
+from aiodocker.containers import DockerContainer
 from aiodocker.volumes import DockerVolume
 from aiodocker.exceptions import DockerError
 from contextlib import asynccontextmanager, suppress
 from dataclasses import dataclass
 
-
-@asynccontextmanager
-async def workdir(
-    docker_client: Docker,
-    name: str,
-    driver: str = 'local',
-):
-    volume = None
-    try:
-        volume = await docker_client.volumes.create({
-            'Name': name,
-            'Driver': driver,
-            'Labels': [],
-        })
-
-        yield volume
-    finally:
-        with suppress(DockerError):
-            if volume:
-                await volume.delete()
-
-
-@dataclass
-class DockerProfile:
-    image: str
-    workdir_mount: str
-    cmd: Sequence[str]
+__all__ = [
+    'DockerProfile',
+    'File',
+    'Limits',
+    'DockerExecutor',
+]
 
 
 @dataclass
@@ -47,33 +33,126 @@ class File:
         else:
             return self.content.encode('utf-8')
 
+
+@dataclass
+class DockerProfile:
+    image: str
+    workdir_mount: str
+    cmd: list[str]
+
+
 @dataclass(frozen=True)
 class Limits:
-
     time: timedelta = timedelta(seconds=1)
     memory_mb: int = 64
     cpu_count: int = 1
     disk_space_mb: int = 256
 
 
+def create_tarball(files: Sequence[File]) -> io.BytesIO:
+    file_obj = io.BytesIO()
+    timestamp = time.time()
+    with tarfile.open(fileobj=file_obj, mode='w') as tarball:
+        for file in files:
+            content = file.content_bytes()
+
+            file_info = tarfile.TarInfo(file.name)
+            file_info.size = len(content)
+            file_info.mtime = timestamp
+
+            tarball.addfile(
+                tarinfo=file_info,
+                fileobj=io.BytesIO(content)
+            )
+
+    return file_obj
+
+
+async def write_files(
+    container: DockerContainer,
+    directory: pathlib.Path,
+    files: Sequence[File],
+) -> None:
+    tarball = create_tarball(files)
+    await container.put_archive(directory, tarball.getvalue())
+
+
 class DockerExecutor:
 
-    def __init__(self, url: str, name_factory: Callable[[], str]) -> None:
+    def __init__(
+        self,
+        url: str = None,
+        name_factory: Callable[[], str] = None
+    ) -> None:
         self.docker_client = Docker(url)
-        self.name_factory = name_factory
+        if name_factory:
+            self.name_factory = name_factory
+        else:
+            self.name_factory = lambda: str(uuid.uuid4())
 
-    async def run(
+    async def create_container(
         self,
         profile: DockerProfile,
         files: Sequence[File],
         workdir: DockerVolume,
         limits: Limits = Limits(),
-    ) -> Awaitable:
-        container = await self.docker_client.containers.create({
+        timeout: int = 5
+    ) -> DockerContainer:
+
+        config = {
             'Image': profile.image,
             'Cmd': profile.cmd,
             'Volumes': {
-                profile.workdir_mount: workdir.name
+                workdir.name: {
+                     'bind': profile.workdir_mount,
+                     'mode': 'rw'
+                }
             },
-        }, name=self.name_factory())
+            'WorkingDir': profile.workdir_mount,
+            'User': 'sandbox',
+            'AttachStdin': True,
+            'AttachStdout': True,
+            'AttachStderr': True,
+            'Tty': False,
+            'OpenStdin': True,
+            'StdinOnce': True,
+        }
+
+        task = self.docker_client.containers.create(config, name=self.name_factory())
+
+        container = await asyncio.wait_for(task, timeout)
+
+        await write_files(
+            container=container,
+            directory=profile.workdir_mount,
+            files=files,
+        )
+
+        return container
+
+    async def run(self, container: DockerContainer):
+        await container.start()
+
+    @asynccontextmanager
+    async def workdir(
+        self,
+        name: str = None,
+        driver: str = 'local',
+        timeout: int = 5,
+    ):
+
+        if not name:
+            name = self.name_factory()
+
+        volume = None
+        try:
+            volume = await asyncio.wait_for(self.docker_client.volumes.create({
+                'Name': name,
+                'Driver': driver,
+            }), timeout)
+            yield volume
+        finally:
+            if volume:
+                with suppress(DockerError):
+                    await volume.delete()
 
--- tests/test_docker.py
+++ tests/test_docker.py
@@ -1,18 +1,29 @@
+import io
+import tarfile
 import aiodocker
 import pytest
 
-from runbox.docker.docker_api import workdir
+from runbox.docker.docker_api import(
+   DockerExecutor, create_tarball, File
+)
 
 
 @pytest.mark.asyncio
 @pytest.fixture
 async def docker_client():
-    return aiodocker.Docker()
+    client = aiodocker.Docker()
+    yield client
+    await client.close()
+
+
+@pytest.fixture
+async def docker_executor():
+    return DockerExecutor()
 
 
 @pytest.mark.asyncio
-async def test_workdir(docker_client: aiodocker.Docker):
-    async with workdir(docker_client, 'test-volume') as wd:
+async def test_workdir(docker_executor: DockerExecutor, docker_client: aiodocker.Docker):
+    async with docker_executor.workdir('test-volume') as wd:
         volumes = await docker_client.volumes.list()
         volume_names = [vol["Name"] for vol in volumes["Volumes"]]
 
@@ -24,3 +35,50 @@ async def test_workdir(docker_client: aiodocker.Docker):
     volumes = await docker_client.volumes.list()
     volume_names = [vol["Name"] for vol in volumes["Volumes"]]
     assert wd.name not in volume_names
+
+
+def test_tarball_create_single_file():
+    files = [
+        File(
+            name='main.py',
+            content=(
+                'print("Hello, world!")'
+            )
+        )
+    ]
+
+    tar_file = create_tarball(files)
+    fileobj = io.BytesIO(tar_file.getvalue())
+
+    with tarfile.open(fileobj=fileobj, mode='r') as tarball:
+        assert tarball.getnames() == ['main.py']
+        data = tarball.extractfile('main.py').read().decode('utf-8')
+        assert data == 'print("Hello, world!")'
+
+
+def test_tarball_create_multiple_files():
+    files = [
+        File(
+            name='main.py',
+            content=(
+                'print("Hello, world!")'
+            )
+        ),
+        File(
+            name='input.txt',
+            content=(
+                'important text!'
+            )
+        )
+    ]
+
+    tar_file = create_tarball(files)
+    fileobj = io.BytesIO(tar_file.getvalue())
+
+    with tarfile.open(fileobj=fileobj, mode='r') as tarball:
+        assert tarball.getnames() == ['main.py', 'input.txt']
+        main_py = tarball.extractfile('main.py').read().decode('utf-8')
+        input_txt = tarball.extractfile('input.txt').read().decode('utf-8')
+        assert main_py == 'print("Hello, world!")'
+        assert input_txt == 'important text!'
+
