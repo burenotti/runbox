d66b7df1b6eec6a699e2827e91c9ab432fbc9fef
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2022-04-13 11:48:38 +0300

    📝 Add Sphinx extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,6 +32,8 @@ release = '0.3.1'
 # ones.
 extensions = [
     'sphinx.ext.autosummary',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
