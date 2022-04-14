17b8be24eceb9575261e5ee38e739a078beea7ea
Artem <burenotti@gmail.com>
2022-04-14 23:28:17 +0300

    🚸 Simplified example with c++ program

--- examples/cpp-simple-dividers.py
+++ examples/cpp-simple-dividers.py
@@ -1,36 +1,23 @@
 import asyncio
-import pathlib
 from datetime import timedelta
-from typing import Sequence
 
 from runbox import DockerExecutor
 from runbox.models import DockerProfile, Limits, File
 
-
-class BuildProfile(DockerProfile):
-    image = 'gcc10-sandbox:latest'
-    user: str = 'sandbox'
-    workdir_mount: pathlib.Path = '/sandbox'
-    exec: str = 'g++'
-
-    def cmd(self, files: Sequence[File]) -> list[str]:
-        return ['g++', *map(lambda file: file.name, files), '-o', '/sandbox/build']
-
-
-class RunProfile(DockerProfile):
-    image: str = 'ubuntu:latest'
-    workdir_mount: pathlib.Path = '/opt'
-    user: str = 'root'
-    exec: str = 'g++'
-
-    def cmd(self, _: Sequence[File]) -> list[str]:
-        return ['/opt/build']
-
-
 limits = Limits(time=timedelta(hours=1))
 
-build_profile = BuildProfile()
-run_profile = RunProfile()
+build_profile = DockerProfile(
+    image='gcc10-sandbox:latest',
+    user='sandbox',
+    workdir_mount='/sandbox',
+    cmd_template=['g++', ..., '-o', '/sandbox/build']
+)
+run_profile = DockerProfile(
+    image='ubuntu:latest',
+    user='root',
+    workdir_mount='/opt',
+    cmd_template=['/opt/build']
+)
 
 with open('./src/dividers.cpp') as file:
     content = file.read()
