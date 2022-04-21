6578f61b43c8c5596f70a62b69f0877db51a189a
Artem <burenotti@gmail.com>
2022-04-21 17:39:12 +0300

    📝 Updated cpp-simple-dividers example

--- examples/cpp-simple-dividers.py
+++ examples/cpp-simple-dividers.py
@@ -1,7 +1,7 @@
 import asyncio
 from datetime import timedelta
 
-from runbox import DockerExecutor
+from runbox import DockerExecutor, SandboxBuilder
 from runbox.models import DockerProfile, Limits, File
 
 limits = Limits(time=timedelta(hours=1))
@@ -9,13 +9,13 @@ limits = Limits(time=timedelta(hours=1))
 build_profile = DockerProfile(
     image='gcc10-sandbox:latest',
     user='sandbox',
-    workdir_mount='/sandbox',
+    workdir='/sandbox',
     cmd_template=['g++', ..., '-o', '/sandbox/build']
 )
 run_profile = DockerProfile(
     image='ubuntu:latest',
     user='root',
-    workdir_mount='/opt',
+    workdir='/opt',
     cmd_template=['/opt/build']
 )
 
@@ -28,16 +28,27 @@ async def main():
     executor = DockerExecutor()
 
     async with executor.workdir() as workdir:
-        build_sandbox = await executor.create_container(build_profile, [file], workdir)
+        builder = SandboxBuilder() \
+            .with_limits(limits) \
+            .add_files(file)
+
+        build_sandbox = await builder \
+            .with_profile(build_profile) \
+            .mount(workdir, '/sandbox') \
+            .create(executor)
+
         await build_sandbox.run()
         await build_sandbox.wait()
 
-        run_sandbox = await executor.create_container(run_profile, [], workdir)
+        run_sandbox = await builder \
+            .with_profile(run_profile) \
+            .mount(workdir, '/opt') \
+            .create(executor)
 
-        reader = await run_sandbox.run(stdin=b'354\n')
-        await run_sandbox.wait()
-        data = await reader.read_out()
-        print(data.data)
+    reader = await run_sandbox.run(stdin=b'354\n')
+    await run_sandbox.wait()
+    data = await reader.read_out()
+    print(data.data)
 
     await executor.close()
 
