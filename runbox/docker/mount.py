9a90e1954c44e39cebb652e5a15e3ea2a00b9d6c
Artem <burenotti@gmail.com>
2022-04-21 10:45:17 +0300

    🚧 Added mount model

---
+++ runbox/docker/mount.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+from aiodocker.volumes import DockerVolume
+from pydantic import BaseModel
+
+
+class Mount(BaseModel):
+    volume: DockerVolume
+    bind: Path
+    readonly: bool = False
+
+    class Config:
+        allow_mutation = False
+
+    def dump(self) -> dict:
+        return {
+            "Target": str(self.bind),
+            "Source": self.volume.name,
+            "Type": "volume",
+            "ReadOnly": self.readonly,
+        }
