4e87f7bb36723420e439386a57d1c8add053c00c
Artem <burenotti@gmail.com>
2022-10-29 15:04:07 +0300

    ✅ Test python3_simpified.json pipeline works

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,11 +1,21 @@
 from pathlib import Path
 
+import pytest
+
+from runbox import DockerExecutor
 from runbox.build_stages import BasePipeline
 from runbox.build_stages.pipeline_loaders import (
     load_stages, JsonPipelineLoader,
 )
 from runbox.build_stages.stages import UseSandbox, UseVolume, default_stages
 from runbox.models import DockerProfile, Limits, File
+from test_build_stages import TestSandboxObserver
+from test_docker import docker_executor
+
+
+@pytest.fixture
+def hello_world_observer():
+    return TestSandboxObserver(expected_stdout="Hello, world!\n")
 
 
 def test_can_load_default_stages():
@@ -54,3 +64,21 @@ def test_can_load_pipeline_from_json():
     )
 
     assert pipeline._groups['run'][0].params == expected_pipeline._groups['run'][0].params
+
+
+@pytest.mark.asyncio
+async def test_pipeline_can_run_and_observe_code(
+    docker_executor: DockerExecutor,
+    hello_world_observer: TestSandboxObserver,
+):
+    pipeline = JsonPipelineLoader[BasePipeline](
+        path=Path('./tests/python3_simplified.json'),
+        stage_getter=lambda stage: load_stages(default_stages())[stage],
+        class_=BasePipeline
+    ).load()
+
+    pipeline.with_executor(docker_executor)
+    pipeline.with_observer(hello_world_observer)
+    await pipeline.execute_group('run')
+
+    hello_world_observer.validate()
